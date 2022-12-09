e437884518a483cf3186b3ce901b838b43b16e07
Chrisbchicken <chrsbnjmn@gmail.com>
2022-12-08 18:28:36 -0600

    added to javascript

--- script.js
+++ script.js
@@ -58,7 +58,13 @@ var getPlayerInfo = function (playerId) {
   fetch('https://free-nba.p.rapidapi.com/stats?per_page=100&seasons[]=2022&player_ids[]=' + playerId, stats)
     .then(response => response.json())
     .then(function (data) {
-      for (i = 0; i < 10; i++) {
+      //appends the players name to the #PlayerStats <div>
+      var playerName = document.createElement('div');
+      playerName.textContent = 'Player stats for: ' + data.data[0].player.first_name + ' ' + data.data[0].player.last_name;
+      playerStats.appendChild(playerName)
+
+      //loops through 5 games and appends the stats to #PlayerStats <div>
+      for (i = 0; i < 5; i++) {
         var points = document.createElement('div');
         var assists = document.createElement('div');
         var fgpercentage = document.createElement('div');
@@ -66,48 +72,83 @@ var getPlayerInfo = function (playerId) {
         points.textContent = 'points: ' + data.data[i].pts;
         assists.textContent = 'assists: ' + data.data[i].ast;
         fgpercentage.textContent = 'field goal percentage: ' + Math.floor(data.data[i].fg_pct * 100) + '%';
-        
+
         playerStats.appendChild(points);
         playerStats.appendChild(assists);
         playerStats.appendChild(fgpercentage);
+      }
+      //stores players name in a variabele
+      var name = data.data[0].player.first_name + ' ' + data.data[0].player.last_name;
+      //gets team name and stores it in a variable
+      var playerTeam = data.data[0].team.name;
+      //runs Api call with the team name and player name as parameters
+      getGameOdds(playerTeam, name);
+    })
+    .catch(err => console.error(err));
+}
 
-        console.log(data.data[i]);
+var getGameOdds = function (playerTeam, name) {
+  const gameOdds = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'ab81f85c68msh657a1c030dd42a6p1d14e5jsnad845cc2dca8',
+      'X-RapidAPI-Host': 'odds.p.rapidapi.com'
+    }
+  };
+
+  fetch('https://odds.p.rapidapi.com/v4/sports/basketball_nba/odds?regions=us&oddsFormat=decimal&markets=h2h%2Cspreads', gameOdds)
+    .then(response => response.json())
+    .then(function(data) {
+      //console.log(data[0].away_team, data[0].home_team);
+      //console.log(data[0].bookmakers[0].markets[1].outcomes[0].name);
+      //console.log(data[0].bookmakers[0].markets[1].outcomes[0].point);
+      for (i = 0; i < data.length; i++) {
+        if (data[i].away_team.includes(playerTeam) || data[i].home_team.includes(playerTeam)) {
+          var teamOdds = document.createElement('div');
+          teamOdds.textContent = name + 's' + ' team odds: ' + data[i].bookmakers[0].markets[1].outcomes[0].point;
+          playerStats.appendChild(teamOdds);
+        }  else {
+          getPreviousGames();
+        }
       }
     })
     .catch(err => console.error(err));
 }
 
-
-
-
-userFormEl.addEventListener('submit', formSubmitHandler); userFormEl.addEventListener('submit', formSubmitHandler);
-
-
-
-/*const gameOdds = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': 'ab81f85c68msh657a1c030dd42a6p1d14e5jsnad845cc2dca8',
-    'X-RapidAPI-Host': 'odds.p.rapidapi.com'
-  }
-};
- 
-fetch('https://odds.p.rapidapi.com/v4/sports/basketball_nba/odds?regions=us&oddsFormat=decimal&markets=h2h%2Cspreads&dateFormat=iso', gameOdds)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
-
-  /*const stats = {
+var getPreviousGames = function () {
+  const gameOdds = {
     method: 'GET',
     headers: {
       'X-RapidAPI-Key': 'ab81f85c68msh657a1c030dd42a6p1d14e5jsnad845cc2dca8',
-      'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
+      'X-RapidAPI-Host': 'odds.p.rapidapi.com'
     }
   };
-  
-  fetch('https://free-nba.p.rapidapi.com/players?search=', stats)
+
+  fetch('https://odds.p.rapidapi.com/v4/sports/basketball_nba/odds/?regions=us&oddsFormat=decimal&markets=h2h%2Cspreads', gameOdds)
     .then(response => response.json())
-    .then (function(data) {
+    .then(function(data) {
       console.log(data);
-  })
-    .catch(err => console.error(err));*/
+    })
+    .catch(err => console.error(err));
+}
+
+userFormEl.addEventListener('submit', formSubmitHandler);
+
+
+
+
+
+/*const stats = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'ab81f85c68msh657a1c030dd42a6p1d14e5jsnad845cc2dca8',
+    'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
+  }
+};
+ 
+fetch('https://free-nba.p.rapidapi.com/players?search=', stats)
+  .then(response => response.json())
+  .then (function(data) {
+    console.log(data);
+})
+  .catch(err => console.error(err));*/
