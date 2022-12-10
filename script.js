572df8b93509dc172fb05d21c3f7197036da8473
Chrisbchicken <chrsbnjmn@gmail.com>
2022-12-10 10:25:19 -0600

    javascript

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var playerInputEl = document.querySelector('#player')
 var userFormEl = document.querySelector('#user-form');
 var playerInfo = document.querySelector('.subtitle')
 var playerStats = document.querySelector('#Playerstats')
+var teamOdds;
+var oddsExplanation;
 /*fetch(requestUrl)
 .then(function (response) {
     return response.json();
@@ -63,17 +65,22 @@ var getPlayerInfo = function (playerId) {
 
       //loops through 5 games and appends the stats to #PlayerStats <div>
       for (i = 0; i < 5; i++) {
+        var number = i + 1;
         var points = document.createElement('div');
         var assists = document.createElement('div');
         var fgpercentage = document.createElement('div');
+        var gameNumber = document.createElement('div');
 
         points.textContent = 'points: ' + data.data[i].pts;
         assists.textContent = 'assists: ' + data.data[i].ast;
         fgpercentage.textContent = 'field goal percentage: ' + Math.floor(data.data[i].fg_pct * 100) + '%';
+        gameNumber.textContent = 'game: ' + number;
 
+        playerStats.appendChild(gameNumber);
         playerStats.appendChild(points);
         playerStats.appendChild(assists);
         playerStats.appendChild(fgpercentage);
+       
       }
       //stores players name in a variabele
       var name = data.data[0].player.first_name + ' ' + data.data[0].player.last_name;
@@ -96,57 +103,35 @@ var getGameOdds = function (playerTeam, name) {
 
   fetch('https://odds.p.rapidapi.com/v4/sports/basketball_nba/odds?regions=us&oddsFormat=decimal&markets=h2h%2Cspreads', gameOdds)
     .then(response => response.json())
-    .then(function(data) {
-      //console.log(data[0].away_team, data[0].home_team);
-      //console.log(data[0].bookmakers[0].markets[1].outcomes[0].name);
-      //console.log(data[0].bookmakers[0].markets[1].outcomes[0].point);
+    .then(function (data) {
+      console.log(data);
+      oddsExplanation = document.createElement('div');
+      oddsExplanation.textContent = 'How to read odds: The point spread - also called "the line" or "the spread" - is used as a margin to handicap the favorite team. For betting purposes, the oddsmaker predicts that the favored team will win by a certain number of points. This number of points is the point spread. The favorite is always indicated by a minus sign (e.g. -5.5) and the underdog by a plus sign (e.g.+5.5). If you bet on the favorite, you win your bet if the favorite wins AND their margin of victory is greater than the point spread. If you bet on the underdog, you win if the underdog wins, ties, or if the favored team wins but fails to exceed the point spread.'
+      playerStats.appendChild(oddsExplanation)
       for (i = 0; i < data.length; i++) {
-        console.log(data);
         if (data[i].away_team.includes(playerTeam) || data[i].home_team.includes(playerTeam)) {
-          var teamOdds = document.createElement('div');
-          teamOdds.textContent = name + 's' + ' team odds: ' + data[i].bookmakers[0].markets[1].outcomes[0].point;
+          teamOdds = document.createElement('div');
+          teamOdds.textContent = name + ' team odds: ' + data[i].bookmakers[0].markets[1].outcomes[0].point;
           playerStats.appendChild(teamOdds);
-        }  
+        } 
+      }
+      if (!teamOdds) {
+        oddsExplanation.textContent = '';
+        noGames(name);
       }
     })
     .catch(err => console.error(err));
 }
 
-var getPreviousGames = function () {
-  const gameOdds = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': 'ab81f85c68msh657a1c030dd42a6p1d14e5jsnad845cc2dca8',
-      'X-RapidAPI-Host': 'odds.p.rapidapi.com'
-    }
-  };
-
-  fetch('https://odds.p.rapidapi.com/v4/sports/basketball_nba/odds/?regions=us&oddsFormat=decimal&markets=h2h%2Cspreads', gameOdds)
-    .then(response => response.json())
-    .then(function(data) {
-      console.log(data);
-    })
-    .catch(err => console.error(err));
+var noGames = function (name) {
+  teamOdds = document.createElement('div');
+  teamOdds.textContent = name + ' Does not have any games today';
+  playerStats.appendChild(teamOdds);
 }
-console.log("yo");
+
 
 userFormEl.addEventListener('submit', formSubmitHandler);
 
 
 
 
-
-/*const stats = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': 'ab81f85c68msh657a1c030dd42a6p1d14e5jsnad845cc2dca8',
-    'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
-  }
-};
- 
-fetch('https://free-nba.p.rapidapi.com/players?search=', stats)
-  .then(response => response.json())
-  .then (function(data) {
-    console.log(data);
-})
-  .catch(err => console.error(err));*/
